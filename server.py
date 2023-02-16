2429c73974b8856ada4e394ae179ed65c557be9b
Gurbax <122194475+gurbax-lol@users.noreply.github.com>
2023-02-16 08:12:09 +0530

    Clarify requirements for render_template function

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html")  # Do not add the templates parent directory, Flask will automatically look for index.html in the templates directory
 
 
 if __name__ == "__main__":
